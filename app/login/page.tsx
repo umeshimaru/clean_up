f5cc328103cf37581845f24520afcbba88224f4f
umeshimaru <amtgptwmjgad@yahoo.co.jp>
2026-02-09 20:49:14 +0900

    動的レンダリング

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 import LoginCard from '@/components/LoginCard'
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
